1e98409e57a8df4c03401266619baa8c8f32cab5
Rodrigo Mota <rodrigommota16@gmail.com>
2022-11-13 17:51:52 -0300

    Atualização de lab ansible

---
+++ Jenkins/vangrant/provision.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+yum install epel-release -y
+yum install wget git -y
+
+sudo wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo install java-11-openjdk-devel -y
+sudo yum installsudo  jenkins -y
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systenctl daemon-reload
+sudo service jenkins start
+
+
+
+
+# install docker e do compose
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
